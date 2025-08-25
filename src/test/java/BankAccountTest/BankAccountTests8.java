5e2a6607681cb6d662dec2cc0ccde106f4bbe9d9
salmanovilkin <ilkin.salmanov.896@gmail.com>
2025-08-25 17:10:09 +0400

    BankAccountTests8 ve  BankAccountTests9 update olundu

--- src/test/java/BankAccountTest/BankAccountTests8.java
+++ src/test/java/BankAccountTest/BankAccountTests8.java
@@ -1,4 +1,36 @@
 package BankAccountTest;
 
-public class BanckAccountTest8 {
+import org.example.BankAccount;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BankAccountTests8 {
+    BankAccount toAccount;
+    BankAccount fromAccount;
+    @BeforeEach
+    void data() {
+        fromAccount = new BankAccount("12345","Əli Məmmədov",1000.0);
+        toAccount = new BankAccount("1234","ilkin  Salmanov",0.0);
+    }
+
+    @Test
+    @DisplayName("Transfer metodu test")
+    @Disabled("Metod hazr deyl")
+    void transferTest() {
+        fromAccount.transfer(toAccount, 200);
+        // Balansları düzgün yoxlayırıq
+        assertEquals(800, fromAccount.getBalance()); // 1000 - 200
+        assertEquals(200, toAccount.getBalance());   // 0 + 200
+
+        System.out.println("Test uğurlu oldu");
+    }
+
+
+
+
+
 }
--- src/test/java/BankAccountTest/BankAccountTests9.java
+++ src/test/java/BankAccountTest/BankAccountTests9.java
@@ -1,4 +1,60 @@
 package BankAccountTest;
 
+import org.example.BankAccount;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class BankAccountTests9 {
+    BankAccount account;
+    BankAccount toAccount;
+
+    @BeforeEach
+    void setup(){
+        account = new BankAccount("12345","Əli Məmmədov",0.0);
+    }
+
+    @DisplayName("Nested istifadə edərək testləri qruplara ayırmaq")
+    @Nested
+    //Nested yaradanda class yaradiriq ve () işarəsini qoymuruq
+    class Operations {
+
+        @Test
+        @DisplayName("Deposit operations")
+        void depositOperations(){
+            account.deposit(500.0);
+            assertEquals(500.0,account.getBalance(),"Doğru məbləğ daxil edin");
+            System.out.println("Balans" + account.getBalance());
+            System.out.println("Test passed:Deposit operations");
+
+        }
+
+        @Test
+        @DisplayName("Withdraw operations")
+        void withdraw(){
+            account.deposit(500.0);
+
+            account.withdraw(200.0);
+            assertEquals(300.0,account.getBalance(),"Balansda kifayət qədər məbləğ yoxdur");
+
+            System.out.println("Balans" + account.getBalance());
+            System.out.println("Test passed:Withdraw operations");
+
+        }
+        @Test
+        @DisplayName("Balans 0 olarsa transfer olmur")
+        void zeroBalnceTransfer(){
+
+            Exception exception = assertThrows(IllegalArgumentException.class,()-> {
+
+                account.transfer(toAccount,100);
+
+            });
+            assertEquals("Balans kifayet etmir",exception.getMessage());
+
+        }
+    }
+
+
 }
